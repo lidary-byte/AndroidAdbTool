366d5dacf02373a40acc3f189345f89e85f1bbac
ohdu <lidaryl@163.com>
2023-11-03 14:08:03 +0800

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.0"
+    kotlin("jvm")
+    id("org.jetbrains.compose")
 }
 
 group = "com.ohdu"
@@ -21,9 +19,7 @@ dependencies {
     implementation("com.squareup.okio:okio:3.2.0")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-}
+
 
 compose.desktop {
     application {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-    
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
 }
 rootProject.name = "AndroidAdbTool"
 
